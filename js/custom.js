fa6355f40aae5c462ed8a5e69345b83461929d11
Leo <lfondriest@college.harvard.edu>
2017-12-11 19:39:29 -0500

    Test Upload

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+$("#box-1").click(function(){
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#vis-1").offset().top + offset
+    }, 2000);
+});
+
+$("#box-2").click(function(){
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#vis-2").offset().top + offset
+    }, 2000);
+});
+
+$("#box-3").click(function(){
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#vis-3").offset().top + offset
+    }, 2000);
+});
+
+$("#box-4").click(function(){
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#vis-4").offset().top + offset
+    }, 2000);
+});
+
+$("#box-5").click(function(){
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#vis-5").offset().top + offset
+    }, 2000);
+});
+
+$("#box-6").click(function(){
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#vis-6").offset().top + offset
+    }, 2000);
+});
---
+++ js/main.js
@@ -0,0 +1,25 @@
+// When the DOM is ready
+$(function() {
+
+    // Init ScrollMagic Controller
+    var scrollMagicController = new ScrollMagic();
+
+    // Animation will be ignored and replaced by scene value in this example
+    var tween = TweenMax.to('#animation', 0.5, {
+        backgroundColor: 'rgb(255, 39, 46)',
+        scale: 5,
+        rotation: 360
+    });
+
+    // Create the Scene and trigger when visible
+    var scene = new ScrollMagic.Scene({
+        triggerElement: '#scene',
+        duration: 300 /* How many pixels to scroll / animate */
+    })
+        .setTween(tween)
+        .addTo(scrollMagicController);
+
+    // Add debug indicators fixed on right side
+    scene.addIndicators();
+
+});
